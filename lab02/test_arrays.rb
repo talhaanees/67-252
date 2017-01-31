690728a3f02c9ab8fe380c2b75cecdcdf0c6f179
Talha Anees <manees@andrew.cmu.edu>
2017-01-31 09:48:39 +0000

    Lab 2 files and code

---
+++ lab02/test_arrays.rb
@@ -0,0 +1,11 @@
+# TEST ARRAYS FOR REGEX TESTER
+# ----------------------------
+%w[http://www.google.com apidock.com www.microsoft.com
+http://www.heimann-family.org http://www.kli.org
+http://www.acac.net http://www.cmu.edu http://is.hss.cmu.edu
+www.amazon.co.uk]
+%w[1234567890123456 1234-5678-9012-3456 1234\ 5678\ 9012\ 3456
+1234567890 #1234567890123456 1234|5678|9012|3456]
+# INITIAL REGEX PATTERN FOR REGEX TESTER
+# --------------------------------------
+pattern = /^(http:\/\/)?www\.\w+\.(com|edu|org)$/
