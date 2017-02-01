80cf4a2d1c1e4f54a77b6a16651cb4b2030ad14f
Talha Anees <manees@andrew.cmu.edu>
2017-02-01 15:31:05 +0000

    Final changes made to Lab 2. Error still exists

--- lab02/regex_tester.rb
+++ lab02/regex_tester.rb
@@ -6,24 +6,47 @@ class RegexTester
     
     attr_accessor :pattern
     
-    def statement=(statement)
-        @statement = statement
+    def statements=(arr)
+        begin
+            raise TypeError unless arr.class == Array
+            raise RuntimeError if arr.empty?
+            @statements = arr
+        rescue RuntimeError
+            STDERR.puts "You need to have at least one statement to test against the pattern."
+            add_insult
+        exit
+        rescue TypeError
+            STDERR.puts "You must enter an ARRAY of statements to use this regex_tester."
+            add_insult
+        exit
+        end
+    end
+
+    
+    def statements
+        @statements
     end
     
-    def statement
-        @statement
+    def add_insult
+        STDERR.puts "-------------------------------------"
+        STDERR.puts "As a coding infidel, you are hereby sentenced
+                    to death. The firing squad will be here shortly to carry
+                    out the execution. Please remain seated until they arrive.
+                    Thank you for your cooperation."
     end
     
     def test
-        if pattern_matches? @statement
-            puts "MATCH: #{@statement}"
-        else
-            STDERR.puts "NO MATCH: #{@statement}"
+        for statement in @statements
+            if pattern_matches? @statement
+                puts "MATCH: #{statement}"
+            else
+                STDERR.puts "NO MATCH: #{statement}"
+            end
         end
     end
     
-    def pattern_matches? statement
-        statement.match(@pattern) != nil
+    def pattern_matches? inp
+        inp.match(@pattern) != nil
     end
     
 end
@@ -32,8 +55,11 @@ regex = RegexTester.new
 regex.pattern = /^(http:\/\/)?www\.\w+\.(com|edu|org)$/
 # from test_arrays.rb
 puts regex.pattern
-regex.statement = "http://www.google.com"
-puts regex.statement
+regex.statements = %w[http://www.google.com apidock.com www.microsoft.com
+http://www.heimann-family.org http://www.kli.org
+http://www.acac.net http://www.cmu.edu http://is.hss.cmu.edu
+www.amazon.co.uk]
+puts regex.statements
 
 puts "------"
 regex.test
